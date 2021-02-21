a9f1f1b5806ae6a18a4e3aaacbeb7f18d6e6e071
seoyeongkang <jumprope123@naver.com>
2021-02-21 17:07:06 +0900

    어댑터 클래스 뼈대 작업 마무리

--- app/src/main/java/com/tjoeun/studentlist_20210221/adapters/StudentAdapter.kt
+++ app/src/main/java/com/tjoeun/studentlist_20210221/adapters/StudentAdapter.kt
@@ -1,11 +1,33 @@
 package com.tjoeun.studentlist_20210221.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.tjoeun.studentlist_20210221.R
 import com.tjoeun.studentlist_20210221.datas.Student
 
 class StudentAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Student> ) : ArrayAdapter<Student>(mContext, resId, mList) {
+
+//    student_list_item xml을 가지고 => 코틀린에서 다룰수 있게 도와주는 변수. (inf) 클래스 (LayoutInflater)
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
+
 }
