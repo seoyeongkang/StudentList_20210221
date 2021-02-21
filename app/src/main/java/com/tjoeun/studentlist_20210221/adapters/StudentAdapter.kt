d1dd75d32bc401334b4393ebc45b59141b934d4f
seoyeongkang <jumprope123@naver.com>
2021-02-21 16:51:01 +0900

    학생들을 뿌려주는 Adapter 생성 작업

---
+++ app/src/main/java/com/tjoeun/studentlist_20210221/adapters/StudentAdapter.kt
@@ -0,0 +1,11 @@
+package com.tjoeun.studentlist_20210221.adapters
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.tjoeun.studentlist_20210221.datas.Student
+
+class StudentAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<Student> ) : ArrayAdapter<Student>(mContext, resId, mList) {
+}
--- app/src/main/java/com/tjoeun/studentlist_20210221/datas/Student.kt
+++ app/src/main/java/com/tjoeun/studentlist_20210221/datas/Student.kt
@@ -5,5 +5,4 @@ class Student(
     val birthYear: Int) {
 
 
-
 }
