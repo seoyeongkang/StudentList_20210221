7e4faba84c505354fca7511275cc0b9d73ca9c2e
seoyeongkang <jumprope123@naver.com>
2021-02-28 11:23:20 +0900

    롱클릭 이벤트 변경 - 경고창 - 확인시에만 삭제 진행

--- app/src/main/java/com/tjoeun/studentlist_20210221/MainActivity.kt
+++ app/src/main/java/com/tjoeun/studentlist_20210221/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.tjoeun.studentlist_20210221
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.tjoeun.studentlist_20210221.adapters.StudentAdapter
 import com.tjoeun.studentlist_20210221.datas.Student
 import kotlinx.android.synthetic.main.activity_main.*
@@ -56,11 +58,24 @@ class MainActivity : AppCompatActivity() {
 //            val std = mStudentList[position]
 //            Toast.makeText(this, "${std.name} 길게 눌림", Toast.LENGTH_SHORT).show()
 
-//          목록에서 해당 위치의 학생을 제거
-            mStudentList.removeAt(position)
+//             진짜 삭제를 진행하기 전에, 정말 지울건지? 물어보고 진행
+
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("삭제 확인")
+            alert.setMessage("정말 해당 학생을 삭제하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+
+//                확인 버튼이 눌리면 할 일 => 진짜 삭제 진행
+                //          목록에서 해당 위치의 학생을 제거
+                mStudentList.removeAt(position)
 
 //          리스트뷰의 어댑터에게 알림 전달 => 새로고침 요청
-            mAdapter.notifyDataSetChanged()
+                mAdapter.notifyDataSetChanged()
+
+            })
+
+            alert.setNegativeButton("취소", null)
+            alert.show()
 
 //            true - 롱클릭 전용, false - 전용X, 일반클릭도 처리
             return@setOnItemLongClickListener true
