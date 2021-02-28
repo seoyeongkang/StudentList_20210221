a672fa4f1e13c3dc63d741c5c44ef9e2bdb1139e
seoyeongkang <jumprope123@naver.com>
2021-02-28 10:47:28 +0900

    리스트뷰 클릭 이벤트 추가

--- app/src/main/java/com/tjoeun/studentlist_20210221/MainActivity.kt
+++ app/src/main/java/com/tjoeun/studentlist_20210221/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tjoeun.studentlist_20210221
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.tjoeun.studentlist_20210221.adapters.StudentAdapter
 import com.tjoeun.studentlist_20210221.datas.Student
 import kotlinx.android.synthetic.main.activity_main.*
@@ -36,6 +37,19 @@ class MainActivity : AppCompatActivity() {
 
 //        리스트뷰의 어댑터로써 => mAdapter를 지정.
         studentListView.adapter = mAdapter
+
+//        리스트뷰의 각 줄이 눌렸을 경우 대처
+        studentListView.setOnItemClickListener { parent, view, position, id ->
+
+//            position : 몇번 줄이 눌렸는지 알려줌.
+
+//            클릭된 학생의 이름을 토스트로
+
+            val clickedStudnet = mStudentList[position]
+
+            Toast.makeText(this, clickedStudnet.name, Toast.LENGTH_SHORT).show()
+
+        }
 
     }
 }
