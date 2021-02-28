75e8004ef44df177cc9057e7eefdb68eefd1cb9d
seoyeongkang <jumprope123@naver.com>
2021-02-28 10:10:43 +0900

    어댑터 - getView 함수 수정 (실 데이터 반영)  - 나이는 아직 제대로 계산되지 않음.

--- app/src/main/java/com/tjoeun/studentlist_20210221/adapters/StudentAdapter.kt
+++ app/src/main/java/com/tjoeun/studentlist_20210221/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.tjoeun.studentlist_20210221.R
 import com.tjoeun.studentlist_20210221.datas.Student
 
@@ -25,6 +26,21 @@ class StudentAdapter(
         }
 
         val row = tempRow!!
+
+//        화면에 뿌려지는 row를 일부 수정하고 나서 => return 되도록
+//        몇번째 줄에 뿌릴 row인지 파악하고 상황에 맞게 가공
+
+//        mList에서 위치(position)에 맞는 학생을 먼저 추출
+        val student = mList[position]
+
+//        row변수(xml모양)에서 => 필요한 텍스트뷰/이미지뷰 등등을 꺼내오자(수기작업)
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+
+//        Student의 값을 => 텍스트뷰들에 반영
+        nameTxt.text = student.name
+        ageTxt.text = "(${student.birthYear}세)"
+
 
         return row
 
